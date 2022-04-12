d34a1f77e227d1a2a82ade4c86309dff5dbaee2d
Enrique Cordova <ecordovaj@gmail.com>
2022-04-12 08:39:47 -0400

    Agrega secciones 'Quienes somos' y 'Destacados'

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -1,5 +1,4 @@
 //Destaca el link de navbar que se le hace clic al agregar la clase 'active'
-
 $(document).ready(function(){
     $('.linkNavbar').on('click', function(){        //ejecuta la función al hacer click en uno de los items con clase "linkNavbar"
         $('.linkNavbar').removeClass('active')      //quita la clase "active" a cualquier item de clase "linkNavbar" que ya la posea
