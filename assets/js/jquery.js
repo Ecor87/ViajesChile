2e71e39c9ce992ce8d977a081bfb05a22c5d9823
Enrique Cordova <ecordovaj@gmail.com>
2022-04-12 11:25:05 -0400

    Cambios menores de formato

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -1,7 +1,7 @@
 //Destaca el link de navbar que se le hace clic al agregar la clase 'active'
 $(document).ready(function(){
     $('.linkNavbar').on('click', function(){        //ejecuta la función al hacer click en uno de los items con clase "linkNavbar"
-        $('.linkNavbar').removeClass('active')      //quita la clase "active" a cualquier item de clase "linkNavbar" que ya la posea
+        $('.linkNavbar').removeClass('active');      //quita la clase "active" a cualquier item de clase "linkNavbar" que ya la posea
         $(this).addClass('active');                 //agrega la clase "active" al elemento que se le hizo click
     })
 })
